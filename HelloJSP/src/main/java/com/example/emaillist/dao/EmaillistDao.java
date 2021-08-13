82140ece914415a7cd6caff42c4454fc959e580a
노승은 <seun2230@gmail.com>
2021-08-13 16:09:40 +0900

    Emaillist(Model 1) 수정

--- HelloJSP/src/main/java/com/example/emaillist/dao/EmaillistDao.java
+++ HelloJSP/src/main/java/com/example/emaillist/dao/EmaillistDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.example.emaillist.vo.EmailVo;
 
 public interface EmaillistDao {
-	public List<EmailVo> getlist();
+	public List<EmailVo> getList();
 	public int insert(EmailVo vo);
 	public int delete(Long pk);
 }
