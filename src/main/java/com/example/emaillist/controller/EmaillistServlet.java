9ad492eef5ee39d51bd092a6329492961832e7ac
노승은 <seun2230@gmail.com>
2021-08-17 11:22:36 +0900

    Emaillist Model 2: Delete

--- src/main/java/com/example/emaillist/controller/EmaillistServlet.java
+++ src/main/java/com/example/emaillist/controller/EmaillistServlet.java
@@ -55,7 +55,31 @@ public class EmaillistServlet  extends HttpServlet {
 		String actionName = req.getParameter("a");
 		
 		if ("add".equals(actionName)) {
+			String firstName = req.getParameter("first_name");
+			String lastName = req.getParameter("last_name");
+			String email = req.getParameter("email");
+			
+			// VO 객체 생성
+			EmailVo vo = new EmailVo();
+			vo.setLastName(lastName);
+			vo.setFirstName(firstName);
+			vo.setEmail(email);
+			
 			// INSERT 처리
+			EmaillistDao dao = new EmaillistDaoImpl();
+			int insertedCount = dao.insert(vo);
+			
+			// 처리 후 list페이지로 리다이렉트
+			resp.sendRedirect(req.getContextPath() + "/el");
+		} else if ("delete".equals(actionName)) {
+			// a=delete면
+			Long no = Long.valueOf(req.getParameter("no"));
+			
+			EmaillistDao dao = new EmaillistDaoImpl();
+			int deleteCount = dao.delte(no);
+			
+			// 리스트 페이지로 리다이렉트
+		    resp.sendRedirect(req.getContextPath() + "/el");
 		} else {
 			doGet(req, resp);
 		}
